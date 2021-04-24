9678ff6313f0e13a868c8af5a8d1fc48b1b578a1
iggy <lorente.inaki@gmail.com>
2021-04-24 06:49:43 +0200

    fix know issues

--- README.md
+++ README.md
@@ -1,49 +1,64 @@
 # Custom_Qt5_Rviz2
-In this repository is an example on how to implement the `RenderPanel` of [`rviz2`](https://github.com/ros2/rviz). This is a workaround and it's not the best solution but it's the only way I found to work.
+In this repository there is an example of how to implement the `RenderPanel` of [`rviz2`](https://github.com/ros2/rviz). This is a workaround and it's not the best solution but it's the only way I found that work.
 The two repositories are for distribution `ros2 foxy`.
 For this to work, I made a small changes to the `rviz` source code, these can be found on my [fork](https://github.com/MartiBolet/rviz).
-This example tries to be an ROS2 version of this [tutorial](http://docs.ros.org/en/indigo/api/librviz_tutorial/html/index.html#).
-![Screenshot of MyViz working](./img/screenshot.png)
+This example tries to be a ROS2 version of this [tutorial](http://docs.ros.org/en/indigo/api/librviz_tutorial/html/index.html#).
+
+![Screenshot of MyViz working](./img/screenshot2.png)
+
+
 
 ## Compile & run
+
 To compile the project:
-```
+```bash
 cd $HOME
+# Download
 mkdir myviz_ws && cd myviz_ws
 git clone https://github.com/MartiBolet/rviz
 git clone https://github.com/MartiBolet/Custom_Qt5_Rviz2
 cd rviz
 git checkout -b develop_integration_with_qt origin/develop_integration_with_qt
-
 # Build
-cd $HOME/myrviz_ws
-colcon build --symlink-install
+cd $HOME/myviz_ws
+colcon build
 # Install
 source ./install/setup.bash
 ```
-> After compile rviz for first time, you can add `COLCON_IGNORE` files inside the individual packages of `rviz` to dont recompile all when you make a small change.
+> **TIP when testing**: After compiling rviz for the first time, you can add `COLCON_IGNORE` files inside the individual rviz packages. This will allow you to not recompile all when you make a small change.
 
-To run the window:
+
+
+Run:
+
 ```
 ros2 run myviz myviz
 ```
 
 
+
 ## Error `libGL error: failed to create drawable`
+
 If there is an error like `libGL error: failed to create drawable` and the `RenderPanel` doesn't show anything, try to:
 ```
 export LIBGL_ALWAYS_SOFTWARE=1
 ```
 From this [answer](https://answers.ros.org/question/286985/libgl-error-failed-to-create-drawable/) and this [tutorial](http://wiki.ros.org/rviz/Troubleshooting)
 
+
+
+## Known issues
+
+- When you close the MainWindow from Qt cross icon, it is not properly closed and needs to be killed from terminal.
+
+
+
 ## Contributors:
+
 Co-workers: [@albertarla](https://github.com/albertarla), [@iggyrrieta](https://github.com/iggyrrieta).
 All contributions are welcome and open to discussion.
 
-## Known issues
-* When you close the MainWindow with the 'cross' the terminal executing doesn't finish the program.
-* The `Grid` doesn't change color to yellow or the thickness.
-+ The connect functions from `QSlider` makes the program stop without any error.
+
 
 ## License
 Apache License 2.0
--- src/myviz.cpp
+++ src/myviz.cpp
@@ -3,28 +3,41 @@
 
 MyViz::MyViz(rviz_common::VisualizationFrame* frame,
          rviz_common::ros_integration::RosNodeAbstractionIface::WeakPtr ros_node_abs,
-         QWidget* parent )
-  : QMainWindow( parent )
+         QWidget* parent)
+  : QMainWindow(parent)
   , frame_(frame)
   , ros_node_abs_(ros_node_abs)
 {
-    QLabel* thickness_label = new QLabel( "Line Thickness" );
-    QSlider* thickness_slider = new QSlider( Qt::Horizontal );
-    thickness_slider->setMinimum( 1 );
-    thickness_slider->setMaximum( 100 );
-    QLabel* cell_size_label = new QLabel( "Cell Size" );
-    QSlider* cell_size_slider = new QSlider( Qt::Horizontal );
-    cell_size_slider->setMinimum( 1 );
-    cell_size_slider->setMaximum( 100 );
-    QGridLayout* controls_layout = new QGridLayout();
-    controls_layout->addWidget( thickness_label, 0, 0 );
-    controls_layout->addWidget( thickness_slider, 0, 1 );
-    controls_layout->addWidget( cell_size_label, 1, 0 );
-    controls_layout->addWidget( cell_size_slider, 1, 1 );
 
-    // Set Layout of Render Panel
+    /*-----------------------------
+        CONSTRUCT LAYOUT
+    -----------------------------*/
+    QLabel* thickness_label = new QLabel("Line Thickness");
+    QSlider* thickness_slider = new QSlider(Qt::Horizontal);
+    thickness_slider->setMinimum(1);
+    thickness_slider->setMaximum(100);
+    QLabel* cell_size_label = new QLabel("Cell Size");
+    QSlider* cell_size_slider = new QSlider(Qt::Horizontal);
+    cell_size_slider->setMinimum(1);
+    cell_size_slider->setMaximum(100);
+    QGridLayout* controls_layout = new QGridLayout();
+    controls_layout->addWidget(thickness_label, 0, 0);
+    controls_layout->addWidget(thickness_slider, 0, 1);
+    controls_layout->addWidget(cell_size_label, 1, 0);
+    controls_layout->addWidget(cell_size_slider, 1, 1);
+
+    /*-----------------------------
+        INIT MAIN RVIZ CLASSES
+    -----------------------------*/
+    manager_ = frame_->getManager();
+    manager_-> removeAllDisplays ();
+    render_panel_ = frame_->getRenderPanel();
+
+    /*-----------------------------
+        ADD VISUALIZATION
+    -----------------------------*/
     main_layout = new QVBoxLayout;
-    main_layout->addLayout( controls_layout );
+    main_layout->addLayout(controls_layout);
 
     central_widget = new QWidget();
     main_layout->setSpacing(0);
@@ -32,54 +45,50 @@ MyViz::MyViz(rviz_common::VisualizationFrame* frame,
 
     central_widget->setLayout(main_layout);
     setCentralWidget(central_widget);
+    main_layout->addWidget(render_panel_);
 
-    // connect( thickness_slider, SIGNAL( valueChanged( int )), this, SLOT( setThickness( int )));
-    // connect( cell_size_slider, SIGNAL( valueChanged( int )), this, SLOT( setCellSize( int )));
+    /*-----------------------------
+            SIGNALS &
+    -----------------------------*/
+    connect(thickness_slider, SIGNAL(valueChanged(int)), this, SLOT(setThickness(int)));
+    connect(cell_size_slider, SIGNAL(valueChanged(int)), this, SLOT(setCellSize(int)));
 
+    /*-----------------------------
+          DISPLAYS INIT
+    -----------------------------*/
+    DisplayGrid();
+
+    /*-----------------------------
+            INIT SLIDER
+    -----------------------------*/
     thickness_slider->setValue( 25 );
     cell_size_slider->setValue( 10 );
+    
 }
 
 MyViz::~MyViz()
 {
+    ///TODO: Not it is not closing properly 
     delete frame_;
 }
 
-void MyViz::setApp(QApplication * app){
-    frame_->setApp(app);
+void MyViz::DisplayGrid()
+{
+    grid_ = manager_->createDisplay("rviz_default_plugins/Grid", "adjustable grid", true);
+    assert(grid_!= NULL);
+    grid_->subProp("Line Style")->setValue("Billboards");
+    grid_->subProp("Color")->setValue(QColor(Qt::yellow)); 
 }
 
-void MyViz::initialize(){
-    // Initialize the Rviz2
-    frame_->initialize(ros_node_abs_, "");
-
-    rviz_common::RenderPanel* render_panel_;
-    // render_panel_ = new rviz_common::RenderPanel();
-    // render_panel_->getRenderWindow()->initialize();
-
-    // Get the useful
-    manager_ = frame_->getManager();
-    render_panel_ = frame_->getRenderPanel();
-    
-    // Display the Grid
-    grid_ = manager_->createDisplay("rviz/Grid", "adjustable grid", true);
-    assert( grid_ != NULL );
-    grid_->subProp( "Line Style" )->setValue( "Billboards" );    //This doesnt work
-    grid_->subProp( "Color" )->setValue( QColor( Qt::yellow ) ); //This doesnt work
-
-    // Add to visualization
-    main_layout->addWidget( render_panel_ );
-}
-
-// Slots
-void MyViz::setThickness( int thickness_percent ) {
+      
+void MyViz::setThickness( int thickness_percent) {
     if( grid_ != NULL ) {
-        grid_->subProp( "Line Style" )->subProp( "Line Width" )->setValue( thickness_percent / 100.0f );
+        grid_->subProp("Line Style")->subProp("Line Width")->setValue(thickness_percent / 100.0f);
     }
 }
 
 void MyViz::setCellSize( int cell_size_percent ) {
-    if( grid_ != NULL ) {
-        grid_->subProp( "Cell Size" )->setValue( cell_size_percent / 10.0f );
+    if(grid_ != NULL) {
+        grid_->subProp("Cell Size")->setValue(cell_size_percent / 10.0f);
     }
 }
